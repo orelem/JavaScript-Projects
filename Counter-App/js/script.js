8516abbf2760050d1f61ca35937542e624556728
Andrew Wilsher <103464199+orelem@users.noreply.github.com>
2022-12-15 20:25:54 -0500

    Edit add buttons to html and make them work in js

--- Counter-App/js/script.js
+++ Counter-App/js/script.js
@@ -1,1 +1,22 @@
 let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
+
+function reduce() {
+    count -= 1
+    countEl.innerText = count
+}
+
+function reset() {
+    count = 0
+    countEl.innerText = count
+    console.clear()
+}
+
+function save() {
+    console.log(count)
+}
