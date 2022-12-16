e2569a79e611054f3b1d75b806b5380df551b1cf
Andrew Wilsher <103464199+orelem@users.noreply.github.com>
2022-12-16 01:38:53 -0500

    Add more button functions

--- Counter-App/js/script.js
+++ Counter-App/js/script.js
@@ -1,22 +1,31 @@
 let countEl = document.getElementById("count-el")
+let countScript = document.getElementById("count-script")
 let count = 0
 
 function increment() {
     count += 1
-    countEl.innerText = count
+    countEl.textContent = count
 }
 
 function reduce() {
     count -= 1
-    countEl.innerText = count
+    countEl.textContent = count
 }
 
 function reset() {
     count = 0
-    countEl.innerText = count
+    countEl.textContent = count
     console.clear()
 }
 
 function save() {
+    countScript.textContent += count + " - "
     console.log(count)
+}
+
+function clearCount() {
+    count = 0
+    countEl.textContent = count
+    countScript.textContent = "Previous count:"
+    console.clear()
 }
